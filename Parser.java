e1ebdac6fde344eb9bf5123a33b2e9f937d9fb36
Serik Makzhanov <Serik.Makzhanov@gmail.com>
2023-03-23 15:18:27 +0600

    добавил комментарии

--- Parser.java
+++ Parser.java
@@ -12,19 +12,14 @@ public class Parser {
         List<Tag> tags = new ArrayList<>();
         Matcher matcher = TAG_PATTERN.matcher(input);
         while (matcher.find()) {
-            String tagName = matcher.group(1);
-            String attributes = matcher.group(2);
-            String tagValue = matcher.group(3);
+            String tagName = matcher.group(1);  //извлекаем имя тега
+            String attributes = matcher.group(2); //извлекаем атрибуты тэга
+            String tagValue = matcher.group(3); //извлекаем строку между открытием и закрытием тега
 
-            Tag tag = new Tag();
-            tag.setTagName(tagName);
-                if (tagValue.contains("<")) {
-                    parseTags(tagValue);
-                } else tag.setTagValue(tagValue);
+            Tag tag = new Tag(); //создание нового объекта Tag
+            tag.setTagName(tagName); // заполняем имя тега в объекте Tag
 
-
-
-            // Извлечение атрибутов
+            // Извлечение атрибутов и передача в объект Tag
             Pattern attributePattern = Pattern.compile("([a-zA-Z]+)=\"([^\"]+)\"");
             Matcher attributeMatcher = attributePattern.matcher(attributes);
             while (attributeMatcher.find()) {
@@ -32,6 +27,10 @@ public class Parser {
                 String attributeValue = attributeMatcher.group(2);
                 tag.addAttribute(attributeName, attributeValue);
             }
+
+            if (tagValue.contains("<")) {
+                    parseTags(tagValue);
+            } else tag.setTagValue(tagValue);
 
             // Рекурсивный вызов метода для обработки вложенных тегов
 

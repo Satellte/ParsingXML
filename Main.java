5ac0f3c998c365a8a0e4dfcdb84f3b8d5aea6f68
Serik Makzhanov <Serik.Makzhanov@gmail.com>
2023-03-29 02:19:29 +0600

    переделал

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import static Parcer.Parser2.*;
+//import static Parcer.Parser2.*;
 import static Parcer.Parser.*;
 
 
@@ -13,21 +13,17 @@ public class Main {
     public static void main(String[] args) {
         File inputText = new File("./src/Parcer/test.txt");
         Scanner scanner = null;
-        StringBuilder vivod = new StringBuilder();
         try {
             scanner = new Scanner(inputText);
         } catch (FileNotFoundException e) {
             System.out.println("Не удалось найти файл");
         }
-        int levelOfKey = 0;
         StringBuilder inputString = new StringBuilder();
         while(scanner.hasNextLine()) {
             inputString.append(scanner.nextLine().replace("\n","").replace("    ",""));
         }
         scanner.close();
         ArrayList<String> parentPath = new ArrayList<>();
-        vivod.append(parseTags(inputString.toString(), parentPath));
-//        vivod.append(parseTags1(inputString.toString()));
-        System.out.println(vivod);
+        System.out.println(parseTags(String.valueOf(inputString), parentPath));
     }
 }
--- Parser.java
+++ Parser.java
@@ -3,28 +3,38 @@ import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 public class Parser {
-    private static final Pattern tagPattern = Pattern.compile("<([a-zA-Z0-9]+)([^>]*)>(.*?)</\\1>");
-    public static final Pattern attrPattern = Pattern.compile("([a-zA-Z0-9]+)\\s?=\\s?\"([^\"]+)\">");
 
-    private static final Pattern noValueTagPattern = Pattern.compile("<([a-zA-Z0-9]+)([^>]*)/>");
-
-    public static final Pattern noValuePattern = Pattern.compile("<([a-zA-Z\\s=\"0-9])*/>");
-
+    private static final Pattern tagPattern = Pattern
+            .compile("<([a-zA-Z0-9]+)([^>]*)>(.*?)</\\1>|<([a-zA-Z0-9]+)([^>]*)/>");
+    public static final Pattern attrPattern = Pattern.compile("([a-zA-Z0-9]+)\\s?=\\s?\"([^\"]+)\"");
 
     public static StringBuilder parseTags(String input, ArrayList<String> parentPath) {
         boolean again = false;
+
         StringBuilder outPut = new StringBuilder();
 
         Matcher matcher = tagPattern.matcher(input);
         while (matcher.find()) {
             boolean haveBlankValue = false;
             boolean haveAttribute = false;
+            boolean haveAttribute2 = false;
 
+            StringBuilder tagName = new StringBuilder();
+            StringBuilder attribute = new StringBuilder();
+            StringBuilder tagValue = new StringBuilder();
             StringBuilder attributes = new StringBuilder();
 
-            String tagName = matcher.group(1);  //извлекаем имя тега
-            String attribute = matcher.group(2); //извлекаем атрибуты тэга
-            String tagValue = matcher.group(3);
+
+            if (matcher.group(4) == null){
+                tagName.append(matcher.group(1));
+                attribute.append(matcher.group(2));
+                tagValue.append(matcher.group(3));
+            } else if (matcher.group(1) == null) {
+                haveAttribute2 = true;
+                tagName.append(matcher.group(4));
+                attribute.append(matcher.group(5));
+                tagValue.append("");
+            }
 
             Matcher attributeMatcher = attrPattern.matcher(attribute);
             while (attributeMatcher.find()) {
@@ -34,21 +44,40 @@ public class Parser {
                 attributes.append(attributeName).append(" = \"").append(attributeValue).append("\"\n");
             }
 
-            if (tagValue.contains("<")) {
+            String subValue = String.valueOf(tagValue);
+
+            if (subValue.contains("<")) {
                 again = true;
             }
 
-            if (tagValue.equals("")) {
+            if (subValue.equals("")) {
                 again = false;
                 haveBlankValue = true;
             }
 
-            if (!haveAttribute) {
+            if (!haveAttribute && !haveAttribute2) {
+                outPut.append("Element:\n" + "path = " + tagName + "\n");
+//                    System.out.println("No attributes");
+
+            } else if (again) {
+                outPut.append("Element:\n" + "path = ")
+                        .append(parentPath).append(tagName).append("\n")
+                        .append("attributes: \n")
+                        .append(attributes)
+                        .append("\n");
+//                    System.out.println("again");
+            } else if (!again && !haveBlankValue) {
+//                    System.out.println("again 2");
                 outPut.append("Element:\n" + "path = ")
                         .append(parentPath)
                         .append(tagName)
+                        .append("\n")
+                        .append("value = \"")
+                        .append(tagValue)
+                        .append("\"\nattributes: \n")
+                        .append(attributes)
                         .append("\n");
-            } else if (again) {
+            } else if (!again && haveBlankValue) {
                 outPut.append("Element:\n" + "path = ")
                         .append(parentPath)
                         .append(tagName)
@@ -56,60 +85,21 @@ public class Parser {
                         .append("attributes: \n")
                         .append(attributes)
                         .append("\n");
-            }  else if (!again && !haveBlankValue) {
-            outPut.append("Element:\n" + "path = ")
-                    .append(parentPath)
-                    .append(tagName)
-                    .append("\n")
-                    .append("value = \"")
-                    .append(tagValue)
-                    .append("\"\nattributes: \n")
-                    .append(attributes)
-                    .append("\n");
-        } else if (!again && haveBlankValue) {
-            outPut.append("Element:\n" + "path = ")
-                    .append(parentPath)
-                    .append(tagName)
-                    .append("\n")
-                    .append("attributes: \n")
-                    .append(attributes)
-                    .append("\n");
-        }
-            parentPath.add(tagName);
-            outPut.append(parseTags(tagValue, parentPath));
-            parentPath.remove(tagName);
+//                    System.out.println("Value is blank");
+            } else if (haveAttribute2) {
+                outPut.append("Element:\n" +
+                                "path = " + parentPath + "\n" +
+                                "value = nullEEEEEEEEEEEEEEEEEEEEEEEEee" +
+                                "attributes:\n")
+                        .append(attributes)
+                        .append("\n");}
+//                    System.out.println("No attributes");
+            parentPath.add(String.valueOf(tagName));
+            outPut.append(parseTags(subValue, parentPath));
+            parentPath.remove(String.valueOf(tagName));
+
         }
 
-        Matcher matcher1 = noValueTagPattern.matcher(input);
-        while (matcher1.find()){
-            StringBuilder attributes = new StringBuilder();
-            boolean haveAttribute = false;
-
-            String tagName = matcher1.group(1);  //извлекаем имя тега
-            String attribute = matcher1.group(2); //извлекаем атрибуты тэга
-
-            Matcher attributeMatcher1 = noValuePattern.matcher(attribute);
-            while (attributeMatcher1.find()) {
-                haveAttribute = true;
-                String attributeName = attributeMatcher1.group(1);
-                String attributeValue = attributeMatcher1.group(2);
-                attributes.append(attributeName).append(" = \"").append(attributeValue).append("\"\n");
-            }
-            if(!haveAttribute){
-                outPut.append("Element:\n" + "path = ")
-                        .append(parentPath)
-                        .append(tagName)
-                        .append("\nvalue = null")
-                        .append("\n\n");
-            } else {
-                outPut.append("Element:\n" + "path = ")
-                        .append(parentPath)
-                        .append(tagName)
-                        .append("\nvalue = null")
-                        .append("\nattributes: \n")
-                        .append(attributes);
-            }
-        }
         return outPut;
     }
 }
